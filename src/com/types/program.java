c81726087544fe4466c3271086696984a743aa8f
Harinithirupathi <harinithirupathi55@gmail.com>
2018-05-29 17:32:46 +0530

    eclipse program

---
+++ src/com/types/program.java
@@ -0,0 +1,13 @@
+package com.types;
+
+public class program {
+
+	public static void main(String[]args) {
+		String firstName="Harini";
+		String lastName="Thirupathi";
+		
+		System.out.println(firstName+" "+lastName);
+
+	}
+	}
+
