2bac100309ebff38c962d94849b778a9210e31af
Harinithirupathi <harinithirupathi55@gmail.com>
2018-05-30 10:03:18 +0530

    switch case

---
+++ src/com/types/statement.java
@@ -0,0 +1,65 @@
+package com.types;
+
+public class statement {
+
+	public static void main(String[] args) {
+		 
+		int age=9;
+		switch(age) {
+		case 1:
+		{
+		System.out.println(100);
+		break;
+		}
+		case 2:
+		{
+		System.out.println(200);
+		break;
+		}	
+		
+		case 3:
+		{
+		System.out.println(300);
+		break;
+		}
+		case 4:
+		{
+		System.out.println(400);
+		break;
+		}
+		case 5:
+		{
+		System.out.println(500);
+		break;
+		}
+		case 6:
+		{
+		System.out.println(600);
+		break;
+		}
+		case 7:
+		{
+		System.out.println(700);
+		break;
+		}
+		case 8:
+		{
+		System.out.println(800);
+		break;
+		}
+		case 9:
+		{
+		System.out.println(900);
+		break;
+		}
+		default:
+		{
+			System.out.println("default");
+			break;
+		}
+	}
+
+
+}
+}
+
