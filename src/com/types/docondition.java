18bf1d4adb85bd1c8b10b86d2e5097df29a58933
Harinithirupathi <harinithirupathi55@gmail.com>
2018-05-30 10:40:46 +0530

    do while

---
+++ src/com/types/docondition.java
@@ -0,0 +1,16 @@
+package com.types;
+
+public class docondition {
+	public static void main(String[] args) {
+	int name=50;
+	
+	do {
+		System.out.println(name=+name);
+		name++;
+	}
+	while(name<60);
+	
+	}
+		
+	
+}
