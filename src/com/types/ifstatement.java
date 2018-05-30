4315b3ee9d38ef0fae5ecf725b66275eeff5a961
Harinithirupathi <harinithirupathi55@gmail.com>
2018-05-30 09:29:27 +0530

    if condition using program

---
+++ src/com/types/ifstatement.java
@@ -0,0 +1,17 @@
+package com.types;
+
+public class ifstatement {
+
+public static void main(String[]args) {
+	 
+		int num=25;
+		if(num>20) {
+			
+			System.out.println(10);
+		}
+		else {
+			System.out.println(30);
+	}
+}
+}
+
