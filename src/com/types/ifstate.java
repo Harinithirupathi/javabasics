33e9f6582f3aeec96006fbbf5b9b50e1b2858c22
Harinithirupathi <harinithirupathi55@gmail.com>
2018-05-30 09:40:51 +0530

    else using

---
+++ src/com/types/ifstate.java
@@ -0,0 +1,20 @@
+package com.types;
+
+public class ifstate {
+	public static void main(String[]args) {
+		 
+		int age=25;
+		if(age>30) {
+			
+			System.out.println(false);
+		}
+		else {
+			System.out.println(true);
+	}
+		
+		
+		
+}
+}
+
+
