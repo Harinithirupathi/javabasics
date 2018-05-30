1ebf0e6b73d3efa2bab66600e5541cdfff6929fe
Harinithirupathi <harinithirupathi55@gmail.com>
2018-05-30 10:57:36 +0530

    for loop using

---
+++ src/com/types/forloop.java
@@ -0,0 +1,20 @@
+package com.types;
+
+public class forloop {
+	public static void main(String[] args) {
+		int r=6;
+		
+		for(r=6;r<10;r++) {
+		
+			if(r<9) {
+				System.out.println(true);
+			}else
+			{
+				System.out.println(false);
+		}
+	
+		
+		}
+			
+}
+}
