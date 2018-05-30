abc843e90c580853c195793e5a8268c3c2697ac0
Harinithirupathi <harinithirupathi55@gmail.com>
2018-05-30 10:27:54 +0530

    while

---
+++ src/com/types/Form.java
@@ -0,0 +1,15 @@
+package com.types;
+
+public class Form {
+	public static void main(String[] args) {
+		 
+		int age=7;
+		while(age<25) {
+		
+		
+			System.out.println(age);
+			age++;
+		}
+	
+    }
+}
