713871e1d310423230dc2d914ab4d29ee640d324
Harinithirupathi <harinithirupathi55@gmail.com>
2018-06-01 17:25:03 +0530

    sample

---
+++ src/com/types/Accending.java
@@ -0,0 +1,23 @@
+package com.types;
+
+public class Accending {
+    public static void main (String[] args) 
+    {
+int[] arr;
+arr = new int[10];
+arr[0]=99;
+arr[1]=20;
+arr[2]=90;
+arr[3]=30;
+arr[4]=20;
+arr[5]=50;
+arr[6]=70;
+arr[7]=67;
+arr[8]=88;
+arr[9]=70;
+
+for (int i = 0; i < arr.length; i++) {
+System.out.println("i " + " = "+ arr[i]);          
+}
+}
+}
