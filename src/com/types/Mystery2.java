0432aec71aba41d825bf7895d15e154e982bee23
Harinithirupathi <harinithirupathi55@gmail.com>
2018-06-06 12:20:37 +0530

    Mystery2

---
+++ src/com/types/Mystery2.java
@@ -0,0 +1,18 @@
+package com.types;
+
+public class Mystery2 {
+	
+		public static void main(String[] arg)
+		{
+			int count=1;
+			while ( count <= 10 )
+			{
+			 System.out.println( count % 2 == 1 ? "****" : "++++++++" );
+			 ++count;
+			 } // end while
+
+			
+		}
+
+	}
+
