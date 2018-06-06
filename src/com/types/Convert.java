9b543d8d48df1b5610c736edd26dbdc66fbf6042
Harinithirupathi <harinithirupathi55@gmail.com>
2018-06-06 12:32:35 +0530

    convert

---
+++ src/com/types/Convert.java
@@ -0,0 +1,12 @@
+package com.types;
+public class Convert {
+	public static void main(String a[]){ 
+		int i;
+double sum=0;
+   for(i=0;i<=5;i++)
+   {
+	sum=sum+Double.parseDouble(a[i]); 
+   }  
+	    System.out.println(sum);
+}
+}
