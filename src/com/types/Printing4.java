33e3d2e84901891fe6d94ef6d5f8001a935a1e67
Harinithirupathi <harinithirupathi55@gmail.com>
2018-06-06 12:21:56 +0530

    printing

---
+++ src/com/types/Printing4.java
@@ -0,0 +1,14 @@
+package com.types;
+
+public class Printing4 {
+		public static void main(String[] args)
+		{
+			for(int i=0;i<=10;i++)
+			{
+				for(int j=0;j<=5;j++)
+				System.out.println('@');
+				System.out.println();
+			} 
+			}
+		}
+
