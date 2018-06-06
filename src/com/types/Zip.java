f76d19b3fb643c3a031a717a39fc066a64ff63b6
Harinithirupathi <harinithirupathi55@gmail.com>
2018-06-06 12:48:48 +0530

    program example

---
+++ src/com/types/Zip.java
@@ -0,0 +1,22 @@
+
+package com.types;
+
+public class Zip {
+
+	 public static void main(String args[])
+	    {
+	        int num = 9;
+	        while(num > 0)
+	        {
+	            for(int i=1; i<=num; i++)
+	            {
+	                System.out.print(" "+num+" ");
+	            }
+	            System.out.print("\n");
+	            num--;
+	        }
+
+	    }
+		}
+
+
