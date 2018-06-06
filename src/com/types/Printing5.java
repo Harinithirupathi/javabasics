bc16c6602198534627755357d76a8a573cb87471
Harinithirupathi <harinithirupathi55@gmail.com>
2018-06-06 12:22:29 +0530

    printing

---
+++ src/com/types/Printing5.java
@@ -0,0 +1,19 @@
+package com.types;
+
+public class Printing5 {
+	public static void main(String[] args)
+	{
+		for(int i=1;i<=5;i++)
+	{
+			for(int j=1;j<=3;j++)
+			{
+				for(int k=1;k<=4;k++)
+					System.out.println('x');
+				System.out.println();
+			}
+			System.out.println();
+		}
+	}
+		
+
+}
