766086ba2f03bbb6c7b41af05c4bf9b74923ea13
Harinithirupathi <harinithirupathi55@gmail.com>
2018-06-06 12:35:34 +0530

    increment

---
+++ src/com/types/Increment8.java
@@ -0,0 +1,29 @@
+package com.types;
+
+public class Increment8 {
+		public static void main(String[] args)
+		{
+			int x=1,total=1,y=2;
+					while(x<10)
+					{
+						total += x;
+						++x;
+						}
+					while ( x <= 100 )
+					{
+						total += x;
+						++x;
+						System.out.println(total);
+					}
+				 while (y> 0 )
+				{
+				System.out.println( y );
+				--y;
+
+
+				}
+		}
+	}
+
+
+
