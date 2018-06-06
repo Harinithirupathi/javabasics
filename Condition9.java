8db576d92bd62c70c7717362cab629d75fa39f39
Harinithirupathi <harinithirupathi55@gmail.com>
2018-06-06 12:19:23 +0530

    condition

---
+++ Condition9.java
@@ -0,0 +1,41 @@
+package com.Profile;
+
+public class Condition9 {
+		public static void main(String[] args)
+		{
+			int i = 1;
+			double k;
+			while ( i <= 10 )
+			{
+			++i;
+			}
+			for (k= 0.1; k != 1.0; k += 0.1 )
+			{
+				System.out.println( k );
+			}
+			int n = 2;
+			switch (n)
+			{
+			case 1:
+			System.out.println( "The number is 1" );
+			break;
+			case 2:
+			System.out.println( "The number is 2" );
+			break;
+			default:
+			System.out.println( "The number is not 1 or 2" );
+			break;
+			}
+			
+			
+			
+			
+			
+			
+
+
+		}
+		}
+		
+
+
