b573b33252935cc8e1944732b1a82d0a22b8ebec
Harinithirupathi <harinithirupathi55@gmail.com>
2018-06-06 12:27:34 +0530

    age

---
+++ src/com/types/Age7.java
@@ -0,0 +1,14 @@
+package com.types;
+public class Age7 {
+
+		public static void main(String [] args)
+		{
+
+		  int age=0;
+	      if ( age >= 65 )
+	         System.out.println( "Age is greater than or equal to 65" );
+	     else
+	          System.out.println( "Age is less than 65 ");
+	    }
+	
+}
