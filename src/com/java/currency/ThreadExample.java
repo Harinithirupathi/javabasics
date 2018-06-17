78efc49f4173e765f072ca315fe7e79151106377
Harinithirupathi <harinithirupathi55@gmail.com>
2018-06-17 16:15:47 +0530

    ThreadExample redo

--- src/com/java/currency/ThreadExample.java
+++ src/com/java/currency/ThreadExample.java
@@ -5,6 +5,7 @@ public class ThreadExample extends Thread {
 	
 public ThreadExample(String name) {
 	this.name=name;
+	setName(name);
 	start();
 }
 
@@ -12,13 +13,17 @@ public ThreadExample(String name) {
 public void run() {
 for(int i=0;i<10;i++) {
 	System.out.println(name +" = "+i);
+	try{
+		Thread.sleep(50);
+}catch (InterruptedException e) {
+	e.printStackTrace();
 }
-
+}
 }
 public static void main(String[]args) {
 	ThreadExample one =new ThreadExample("one");
 	ThreadExample two =new ThreadExample("two");
-	System.out.println("Main Done");
+	System.out.println("Main Done"+Thread.currentThread());
 }
 
 
