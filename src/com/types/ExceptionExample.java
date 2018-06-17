d2666637e5b8d440f12b0ab459d74e511b6feedd
Harinithirupathi <harinithirupathi55@gmail.com>
2018-06-17 15:33:11 +0530

    example

---
+++ src/com/types/ExceptionExample.java
@@ -0,0 +1,17 @@
+package com.types;
+
+public class ExceptionExample {
+public ExceptionExample() {
+	int count=10;
+	double d=count=10;
+	int i[]= {11,2,34,4};
+	System.out.println(i[5]);
+}
+	public static void main (String[] args){
+	{
+		ExceptionExample ee=new ExceptionExample();
+		
+	}
+	
+}
+}
---
+++ src/com/types/String.java
@@ -0,0 +1,11 @@
+package com.types;
+
+public class String {
+	public static void main(String args[]){
+	 
+''''''''''''''''''''''''''''	   String s="Sachin Tendulkar";
+	   System.out.println(s.subString(0,4));
+
+}
+}
+
