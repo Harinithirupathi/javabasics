47ce3f1b439fb9a24b59ff1df2c20631b0de9fcc
Harinithirupathi <harinithirupathi55@gmail.com>
2018-06-17 15:24:58 +0530

    Student id

---
+++ src/com/types/Students.java
@@ -0,0 +1,32 @@
+package com.types;
+
+public class Students {
+int id;
+int mark;
+String name;
+public void Students(int id,int mark,String name){
+	this.id=id;
+	this.mark=mark;
+	this.name=name;
+	
+}
+public int getId() {
+	return id;
+}
+public void setId(int id) {
+	this.id = id;
+}
+public int getMark() {
+	return mark;
+}
+public void setMark(int mark) {
+	this.mark = mark;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+
+}
