165823e30ac39990ce253e5583d3d1199765fb66
Harinithirupathi <harinithirupathi55@gmail.com>
2018-06-17 15:26:19 +0530

    StudentInfo

---
+++ src/com/types/StudentInfo.java
@@ -0,0 +1,9 @@
+package com.types;
+
+public class StudentInfo {
+public static void main(String []args) {
+	Students studentss1=new Students(66,88,"YYY");
+	Students studentss2=new Students(55,77,""RRR);
+	Students studentss3=new Students();
+}
+}
