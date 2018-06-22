5c7ad48c9d0e83beb3b9a0e82d6fd3954c17b976
Harinithirupathi <harinithirupathi55@gmail.com>
2018-06-22 11:04:07 +0530

    find Duplicate words

---
+++ src/Duplicates.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+public class Duplicates {
+public static void main(String[]args) {
+	Scanner sc=new Scanner(System.in);
+	String s=sc.nextLine().toLowerCase();
+	String s1[]=s.split(" ");
+	int n=s1.length;
+	int cnt = 0;
+	int flag=0;
+	String s3=" ";
+	int i,j;
+	for( i=0;i<n;i++) {
+		s3=" ";
+	}
+		for( j=i+1;j<n;j++) {
+			if(s1[i].equals(s1[j]) && (!s3.equals(s1[j]))){
+				s3=s1[j];
+				cnt++;
+			}
+			
+	
+System.out.print(cnt);
+}
+}
+}
+
+
